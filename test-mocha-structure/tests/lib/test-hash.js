aad9aaa3dba8d6927cbcec7f6c12e27ba328a8c0
Frederik Held <kontakt@frederikheld.de>
2019-02-06 11:18:37 +0100

    Tidies up example text code

--- test-mocha-structure/tests/lib/test-hash.js
+++ test-mocha-structure/tests/lib/test-hash.js
@@ -61,40 +61,6 @@ describe('hash()', () => {
 
     })
 
-})
-
-/*
-describe('hash()', () => {
-
-    context('called without parameters', () => {
-
-        it('should return a token', () => {
-
-            let token = hash()
-            should.exist(token)
-
-            describe('the returned token', () => {
-
-                it('should be exactly 20 characters long', () => {
-                    token.length.should.equal(20)
-                })
-
-                it('should consist only of [a-zA-Z0-9]', () => {
-                    token.should.match(/^[a-zA-Z0-9]*$/gm)
-
-                    // NOTE: I'm not using ^[a-zA-Z0-9]{20}$ here since the length is already
-                    //       tested in a separate test before.
-                    //       Testing the length again would be repetition and would make it
-                    //       more difficult to update the tests if the length should change.
-                    //       Testing both together would make it more difficult to spot what's
-                    //       wrong if the test fails.
-                })
-
-            })
-
-        })
-    })
-
     it('should return a different token every time it is called', () => {
 
         // create tokens:
@@ -108,8 +74,6 @@ describe('hash()', () => {
             tokens.lastIndexOf(item).should.equal(index)
         })
 
-
     })
 
 })
-*/
