6e1abe3abd074f9751cc7bd4d3d03c82540406b9
Frederik Held <kontakt@frederikheld.de>
2019-02-06 20:52:31 +0100

    Improves example for different implementations
    - puts both implementations back into the same function (but commented out)
      because otherwise I would have had to test both sub-functions to
      achieve 100 % coverage. Actually, different implementations wouldn't
      co-exist anyways.
    - improves explanation

--- test-mocha-structure/lib/hash.js
+++ test-mocha-structure/lib/hash.js
@@ -7,30 +7,18 @@ const crypto = require('crypto')
 function hash(length = 20) {
 
     // You can use different implementations of the
-    // hashing algorithm here. If they meet the requirements
-    // defined by the test for hash(), you are free to refactor
-    // at your own discretion.
-    // If the cryptographic quality of the generated hash
-    // might become relevant in the future, you would add a test
-    // that tests against the required quality. This might lead
-    // to failure of one of the implementations.
+    // hashing algorithm here as long as they meet the
+    // requirements defined in the tests for hash().
+    //
+    // If other requirements will become relevant in the
+    // future (like the quality of the generated hash),
+    // one or both of them might fail.
+    //
+    // But under the eyes of the current tests they
+    // are exactly the same and both valid.
 
-    return hashAlgorithm2(length)
 
-}
-
-function hashAlgorithm1(length) {
-
-    return crypto
-        .randomBytes(length)
-        .toString('base64')     // convert to base64 format
-        .slice(0, length)       // return required number of characters
-        .replace(/\+/g, '0')    // replace '+' with '0'
-        .replace(/\//g, '0')    // replace '/' with '0'
-
-}
-
-function hashAlgorithm2(length) {
+    // implementation A:
 
     const characterSet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 
@@ -44,5 +32,15 @@ function hashAlgorithm2(length) {
     }
 
     return hash
+
+
+    // implementation B:
+
+    // return crypto
+    //     .randomBytes(length)
+    //     .toString('base64')     // convert to base64 format
+    //     .slice(0, length)       // return required number of characters
+    //     .replace(/\+/g, '0')    // replace '+' with '0'
+    //     .replace(/\//g, '0')    // replace '/' with '0'
 
 }
